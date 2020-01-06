9be72f2bf892d313595ede28f2dab7da83253fb6
jesuscabeza <jesus.cabeza@ceiba.com.co>
2020-01-06 16:29:04 -0500

    Agregando botón e ícono dentro de éste

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -14,4 +14,5 @@ export class FilterComponent implements OnInit {
   ngOnInit() {
   }
 
+  openDialog(){}
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class MaterialModule { }
