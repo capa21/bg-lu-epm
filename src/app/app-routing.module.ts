fff4c0be8fd82c9e99ef69f723acce72d75f3984
jesuscabeza <jesus.cabeza@ceiba.com.co>
2020-01-06 15:45:32 -0500

    Agregando el FilterComponent y asignando ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FilterComponent } from './components/filter/filter.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FilterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
